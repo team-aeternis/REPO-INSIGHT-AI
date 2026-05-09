27fde7f77db9ac79ad0604958cb83770cb88ed06
abhishekj665 <jeveneabhi665@gmail.com>
2026-05-09 22:16:26 +0530

    Intial Project Setup for Repo Insight AI project

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import { globalErrorHandler } from './middleware/globalErrorHandler.middleware.js';
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use(globalErrorHandler);
+
+export default app;
---
+++ server/src/utils/Error.util.js
@@ -0,0 +1,7 @@
+export default class ExpressError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+  }
+}
