61ba35c7eca4c80e0389f03a8e4f7db9ccfdb6fc
abhishekj665 <jeveneabhi665@gmail.com>
2026-05-11 01:48:49 +0530

    Add repository cloning and file walking functionality
    - Implemented cloneRepo function to clone GitHub repositories and create local directories.
    - Added walkFiles function to recursively list files in a directory while ignoring specified folders, files, and extensions.
    - Updated createRepository and updateRepository functions to handle repository creation and updates with cloning and file analysis.
    - Added necessary dependencies in package.json and package-lock.json.

--- server/src/services/repository/repository.service.js
+++ server/src/services/repository/repository.service.js
@@ -1,5 +1,10 @@
 import ExpressError from "../../utils/Error.util.js";
 import RepositoryModel from "../../models/Repository.model.js";
+import mongoose from "mongoose";
+
+import { cloneRepo } from "../github/cloneRepo.js";
+import { walkFiles } from "../parser/fileWalker.js";
+
 export const getAllRepositories = async () => {
   try {
   } catch (error) {
@@ -15,6 +20,8 @@ export const getRepositoryById = async (id) => {
 };
 
 export const createRepository = async (repositoryData) => {
+  const transaction = await mongoose.startSession();
+  transaction.startTransaction();
   try {
     const { name, url } = repositoryData;
 
@@ -22,38 +29,121 @@ export const createRepository = async (repositoryData) => {
       throw new ExpressError(400, "Name and URL are required");
     }
 
-    const existingRepo = await RepositoryModel.findOne({ url });
+    const existingRepo = await RepositoryModel.findOne({ githubUrl: url });
+
     if (existingRepo) {
-      throw new ExpressError(400, "Repository with this URL already exists");
+      return {
+        success: false,
+        error: "Repository with the same URL already exists",
+        message: "Repository creation failed",
+        status: 400,
+      };
     }
 
-    const newRepository = await RepositoryModel.create({
-      githubUrl: url,
-      repoName: name,
-      owner: repositoryData.owner || "Unknown",
-    });
+    console.log("Creating repository with data:", repositoryData);
 
+    // create new repository with pending status
+
+    const [newRepository] = await RepositoryModel.create(
+      [
+        {
+          githubUrl: url,
+          repoName: name,
+          owner: repositoryData.owner || "Unknown",
+
+          status: "processing",
+        },
+      ],
+
+      {
+        session: transaction,
+      },
+    );
+
+    // clone repo and get local path
+
+    const { repoPath } = await cloneRepo(url, transaction);
+
+    newRepository.localPath = repoPath;
+    await newRepository.save({ session: transaction });
+
+    // walk through files in the repository to extract useful files for analysis
+
+    const allFiles = walkFiles(repoPath);
+
+    console.log("All files in the repository:", allFiles);
+
+    await transaction.commitTransaction();
+    await transaction.endSession();
     return {
-        success: true,
-        data: newRepository.repoName,
-        message: "Repository created successfully",
-        status: 201
-    }
+      success: true,
+      data: newRepository.repoName,
+      message: "Repository created successfully",
+      status: 201,
+    };
   } catch (error) {
-    throw new ExpressError(error.status, error.message);
+    await transaction.abortTransaction();
+    await transaction.endSession();
+    throw new ExpressError(error.status || 500, error.message);
   }
 };
 
 export const updateRepository = async (id, repositoryData) => {
+  const transaction = await mongoose.startSession();
+  transaction.startTransaction();
   try {
+    const { name, url } = repositoryData;
+
+    if (!name || !url) {
+      throw new ExpressError(400, "Name and URL are required");
+    }
+
+    const existingRepo = await RepositoryModel.findOne({ githubUrl: url });
+    console.log("Existing repository:", existingRepo);
+    if (existingRepo) {
+      console.log("Existing repository:", existingRepo);
+    }
+
+    const updatedRepository = await RepositoryModel.findByIdAndUpdate(
+      id,
+      {
+        githubUrl: url,
+        repoName: name,
+        owner: repositoryData.owner || "Unknown",
+      },
+      { new: true },
+    );
+    await transaction.commitTransaction();
+    await transaction.endSession();
+    return {
+      success: true,
+      data: updatedRepository.repoName,
+      message: "Repository updated successfully",
+      status: 200,
+    };
   } catch (error) {
-    throw new ExpressError(error.status, error.message);
+    await transaction.abortTransaction();
+    await transaction.endSession();
+    throw new ExpressError(error.status || 500, error.message);
   }
 };
 
 export const deleteRepository = async (id) => {
+  const transaction = await mongoose.startSession();
+  transaction.startTransaction();
   try {
+    const deletedRepository = await RepositoryModel.findByIdAndDelete(id);
+    await transaction.commitTransaction();
+    await transaction.endSession();
+    return {
+      success: true,
+      data: deletedRepository.repoName,
+      message: "Repository deleted successfully",
+      status: 200,
+    };
   } catch (error) {
-    throw new ExpressError(error.status, error.message);
+    await transaction.abortTransaction();
+    await transaction.endSession();
+    throw new ExpressError(error.status || 500, error.message);
   }
 };
